e62a67ae590d051668ea2e98e3c7c65e7f1fa1f6
Emissions <unitedrealm@hotmail.com>
2023-06-01 04:43:20 +0100

    Switched to ada-types

--- src/BaseApp.ts
+++ src/BaseApp.ts
@@ -1,5 +1,5 @@
 import Base from "ts-modular-bot-file-design";
-import { Dependency } from "ts-modular-bot-types";
+import { Dependency } from "ada-types";
 import { GeneralEvents, DiscordEvents } from "./enums/CommonEvents.js";
 import { EventEmitter } from "node:events";
 import { EventTypes } from "./enums/EventTypes.js";
