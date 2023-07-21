4317c80aea2612930ff318c8d967739db5ebbef9
therealemissions <me@liammartin.site>
2023-07-21 17:24:55 +0100

    changed to ada-file-design

--- src/BaseApp.ts
+++ src/BaseApp.ts
@@ -1,4 +1,4 @@
-import Base from "ts-modular-bot-file-design";
+import Base from "ada-file-design";
 import { Dependency } from "ada-types";
 import { GeneralEvents, DiscordEvents } from "./enums/CommonEvents.js";
 import { EventEmitter } from "node:events";
